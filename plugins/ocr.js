fdb50c52b61fab465634bfd47a162b4c1962623e
Shahid-Shazz <89721494+shahid-devil@users.noreply.github.com>
2021-11-05 15:57:35 +0530

    `update by Shahid ser 🙈`

--- plugins/ocr.js
+++ plugins/ocr.js
@@ -14,7 +14,7 @@ const tesseract = require("node-tesseract-ocr")
 const langs = require('langs');
 const Language = require('../language');
 const Lang = Language.getString('ocr');
-if (Config.STANDPLK == 'off' || Config.STANDPLK == 'OFF') {
+if (Config.STANDPLK == 'off' || Config.STANDSHAZZ == 'OFF') {
 if (Config.WORKTYPE == 'private') {
 
     MyPnky.addCommand({pattern: 'ocr ?(.*)', fromMe: true, desc: Lang.OCR_DESC}, (async (message, match) => { 
