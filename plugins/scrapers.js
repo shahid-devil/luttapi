8c6ed0db597189f0a5d145d372e0904ebcb8cbda
Shahid-Shazz <89721494+shahid-devil@users.noreply.github.com>
2021-11-07 14:44:26 +0530

    Update scrapers.js

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -855,7 +855,7 @@ else if (config.WORKTYPE == 'public') {
                     });
                 writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,fs.readFileSync('./' + title + '.jpg'),Lang.UPLOADING_SONG, MessageType.image);
+                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG + '\n' + title,MessageType.image);
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {quoted: message.data , mimetype: Mimetype.mp4Audio, ptt: false});
             });
     }));
