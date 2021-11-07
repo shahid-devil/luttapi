8a0166067e8c8686e16f3e6f9e849df2d218fb28
Shahid-Shazz <89721494+shahid-devil@users.noreply.github.com>
2021-11-07 14:45:53 +0530

    Set ayoonn nookkaam 🤧

--- plugins/scrapers.js
+++ plugins/scrapers.js
@@ -855,7 +855,7 @@ else if (config.WORKTYPE == 'public') {
                     });
                 writer.addTag();
 
-                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG + '\n' + title,MessageType.image);
+                reply = await message.client.sendMessage(message.jid,Lang.UPLOADING_SONG + '\n' + title,MessageType.text);
                 await message.client.sendMessage(message.jid,Buffer.from(writer.arrayBuffer), MessageType.audio, {quoted: message.data , mimetype: Mimetype.mp4Audio, ptt: false});
             });
     }));
